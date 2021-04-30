70f6ddece59c96a2f5f3912c3841f224998156d3
Xi Jiang <63157069+Xijiang1997@users.noreply.github.com>
2021-04-29 21:20:26 -0500

    Update MouseOB demo.R

--- MouseOB demo.R
+++ MouseOB demo.R
@@ -14,7 +14,7 @@ start_time <- Sys.time()
 
 # In this example, we just run first 100 genes in this dataset
 
-detect_result <- Boost_HP(count_f[,1:100], loc_f, normalization = 2, clustermethod = 'Mclust')
+detect_result <- Boost_Ising(count_f[,1:100], loc_f, normalization = 'tss', clustermethod = 'MGC')
 
 end_time <- Sys.time()
 
@@ -24,7 +24,7 @@ print(end_time - start_time)
 print(detect_result[1:5,])
 
 # get SE genes
-SE_gene <- rownames(detect_result)[which(detect_result$pvalues_neg_ad< 0.05)
+SE_gene <- rownames(detect_result)[which(detect_result$BF_neg > 150)
 print(SE_gene)
 
 # number of detected SE genes 
